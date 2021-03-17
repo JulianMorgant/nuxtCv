2fa2d5d2940638dfba7022a8ae9632c830ff9953
Julian MORGANT <julian.morgant@yahoo.fr>
2021-03-17 15:32:17 +0100

    add component SkillCard

---
+++ components/SkillCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card text-white bg-dark mb-3" style="max-width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ name }}</h5>
+      <p class="card-text">{{ text }}</p>
+      <button @click="$emit('update', name, text)" class="btn btn-primary">update</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SkillCard",
+  props: ["name", "text"],
+};
+</script>
+
--- pages/dev.vue
+++ pages/dev.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
-      <h1 class="display-1">Dev</h1>
+    <h1 class="display-1">Dev</h1>
+
+    <h2>SKILLS</h2>
+    <div class="card-deck">
+     
+ <skill-card v-for="skill in skills" v-bind:key="skill.id" v-bind:name="skill.name" v-bind:text="skill.text" v-on:update="updateSkillCard"></skill-card> 
+     <!-- Meme chose dessous -->
+
+     <!--    <skill-card v-for="skill in skills" v-bind="skill" v-on:update="updateSkillCard"/> -->
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    data(){
-        return{
-            skills: [
-                {name: 'JavaScript', text: 'dev en javascript back-end et front-end'},
-                {name: 'NodeJS', text: 'Back-end node js'},
-                {name: 'Nuxt', text: 'Ecosystème Nuxt'}
-                
-            ]
 
-        }
-    }
-}
+import SkillCard from '../components/SkillCard.vue';
+
+export default {
+    components: {
+        SkillCard
+    },
+  data() {
+    return {
+      skills: [
+        { id: "1", name: "JavaScript", text: "dev en javascript back-end et front-end" },
+        { id: "2", name: "NodeJS", text: "Back-end node js" },
+        { id: "3", name: "Nuxt", text: "Ecosystème Nuxt" },
+      ],
+    };
+  },
+  methods : {
+      updateSkillCard(name,text) {
+          console.log (`update ${name} - ${text}`)
+      }
+  }
+};
 </script>
 
 <style scoped>
