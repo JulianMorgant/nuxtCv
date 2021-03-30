59862ab343372bb22e83285c1465732fddc79e1c
Julian MORGANT <julian.morgant@yahoo.fr>
2021-03-30 12:44:02 +0200

    improve FiveStars Component

--- components/FiveStars.vue
+++ components/FiveStars.vue
@@ -1,56 +1,80 @@
 <template>
   <div class="stars">
     <form action="">
-      <input class="star star-5" id="star-5" type="radio" name="star" />
-      <label class="star star-5" for="star-5"></label>
-      <input class="star star-4" id="star-4" type="radio" name="star" />
-      <label class="star star-4" for="star-4"></label>
-      <input class="star star-3" id="star-3" type="radio" name="star" />
-      <label class="star star-3" for="star-3"></label>
-      <input class="star star-2" id="star-2" type="radio" name="star" />
-      <label class="star star-2" for="star-2"></label>
-      <input class="star star-1" id="star-1" type="radio" name="star" />
-      <label class="star star-1" for="star-1"></label>
+      <label class="title">{{starsData.label}}</label>
+      </BR>
+      <input class="star star-5" id="star[5]" type="radio" name="star" v-model="starsData.number" value = "5" disabled/>
+      <label class="star star-5" for="star[5]"></label>
+      <input class="star star-4" id="star4" type="radio" name="star" v-model="starsData.number" value = "4" disabled/>
+      <label class="star star-4" for="star4"></label>
+      <input class="star star-3" id="star[3]" type="radio" name="star" v-model="starsData.number" value = "3" disabled/>
+      <label class="star star-3" for="star[3]"></label>
+      <input class="star star-2" id="star[2]" type="radio" name="star" v-model="starsData.number" value = "2" disabled/>
+      <label class="star star-2" for="star[2]"></label>
+      <input class="star star-1" id="star[1]" type="radio" name="star" v-model="starsData.number" value = "1" disabled/>
+      <label class="star star-1" for="star[1]"></label>
     </form>
+    <!-- <div>{{starsData}}</div> -->
     <link
       rel="stylesheet"
       href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
     />
   </div>
-</template>
+</template>   
 
 <script>
-
 export default {
-  name : "FiveStars",
-  props: ["starsNumber", "starsLabel"],
+  name: "FiveStars",
+  props: ["starsData"],
   data() {
+    return {};
+  },
 
-   return {
-      
-      }
-    }
-
+  method: {
+    mounted() {
+    },
+  },
 };
+
 </script>
 
 <style scoped>
+
+.title {
+  margin: 0px;
+  float: left;
+  padding: 1px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  font-size: 12px;
+
+}
+
 div.stars {
-  width: 270px;
+  width: 220px;
+  height: auto;
   display: inline-block;
+  /* background-color: #444; for test*/
+  margin: 0px;
+  padding: 0px;
 }
 
 input.star {
   display: none;
+
 }
 
 label.star {
+  margin: 0px;
   float: right;
-  padding: 10px;
-  font-size: 36px;
+  padding: 5px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  font-size: 24px;
   color: #444;
   transition: all 0.2s;
 }
+
 
 input.star:checked ~ label.star:before {
   content: "\f005";
